739780033bf20aa5562da3d638f5053bf7f50417
dogomayana <thomasstephen252@gmail.com>
2022-06-02 11:01:03 +0000

    with logo

--- myScript.js
+++ myScript.js
@@ -26,12 +26,3 @@ window.onload= function(){
     document.getElementById('call').innerHTML="Requirements";
     };
 
-    setInterval(myFunct, 1000);
-
-//  function myFunct() {
-//   let d = new Date();
-//   document.getElementById("demo").innerHTML=
-//   d.getHours() + ":" +
-//   d.getMinutes() + ":" +
-//   d.getSeconds();
-// }
