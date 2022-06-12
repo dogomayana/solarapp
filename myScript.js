578be9fc6e4392f3bd09ec541ff248038b4f216d
dogomayana <thomasstephen252@gmail.com>
2022-06-12 16:47:05 +0000

    v1.0.2

--- myScript.js
+++ myScript.js
@@ -18,18 +18,22 @@ document.onkeydown = function (e) {
 
         const totalLoad = load * hrs;
         const solarPanel = Math.round(totalLoad / 5);
-        const batteryBank = Math.round(totalLoad*2 / 12);
+        const batteryBank = Math.round(totalLoad/ 12*2);
         const chargeController = Math.round(solarPanel /12);
         const inverterW = Math.round(load * 1.25 / 0.8);
         // const loadVa = Math.round(load / 0.8);
         const inverterVa = Math.round(totalLoad/ 0.8);
-        const kva = Math.round(inverterVa/1000)
+        // const kva = Math.round(inverterVa/1000)
+        const kv = (inverterVa/1000)
+        const ka = kv.toFixed(1)
 
     document.getElementById('sPanel').innerHTML="You will need"+" "+solarPanel+"w"+" "+"solar panel";
     document.getElementById('batbank').innerHTML="You will need"+" "+batteryBank+"ah"+" "+"12v"+" "+"battery";
     document.getElementById('chargecontrol').innerHTML="You will need"+" "+chargeController+"amps"+" "+"charge controller";
     document.getElementById('inverter').innerHTML="You will need"+" "+inverterW+"watts"+" "+"Inverter" + " " + "OR";
-    document.getElementById('inverterV').innerHTML="You will need"+" "+kva+"KVA"+" "+"Inverter";
+    // document.getElementById('inverterV').innerHTML="You will need"+" "+kva+"KVA"+" "+"Inverter";
+    document.getElementById('inverterVb').innerHTML="You will need"+" "+ka+"KVA"+" "+"Inverter";
     document.getElementById('call').innerHTML="Requirements";
+    document.getElementById('inquire').innerHTML="For more enquiry on specific products to buy, pls chat 08106264634 on whatsapp.";
     };
 
