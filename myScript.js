7e430c7f635580ed4b5903d1bc3569ddface13fc
dogomayana <thomasstephen252@gmail.com>
2022-06-13 16:13:57 +0000

    v1.0.3

--- myScript.js
+++ myScript.js
@@ -10,6 +10,10 @@ document.onkeydown = function (e) {
         e.preventDefault();
     }
 }
+function autoRefresh() {
+    window.location = window.location.href;
+}
+setInterval('autoRefresh()', 720000);
 
     function myFunction(){
 
@@ -21,18 +25,22 @@ document.onkeydown = function (e) {
         const batteryBank = Math.round(totalLoad/ 12*2);
         const chargeController = Math.round(solarPanel /12);
         const inverterW = Math.round(load * 1.25 / 0.8);
-        const inverterVa = (totalLoad/ 0.8);
+        const inverterVa = Math.round((load/ 0.8)*1.5);
         const kv = (inverterVa/1000)
-        const ka = kv.toFixed(1)
+        const kva = kv.toFixed(1)
 
-    document.getElementById('sPanel').innerHTML="You will need"+" "+solarPanel+"w"+" "+"solar panel";
-    document.getElementById('batbank').innerHTML="You will need"+" "+batteryBank+"ah"+" "+"12v"+" "+"battery";
-    document.getElementById('chargecontrol').innerHTML="You will need"+" "+chargeController+"amps"+" "+"charge controller";
-    document.getElementById('inverter').innerHTML="You will need"+" "+inverterW+"watts"+" "+"Inverter" + " " + "OR";
-    // document.getElementById('inverterV').innerHTML="You will need"+" "+kva+"KVA"+" "+"Inverter";
-    document.getElementById('inverterVb').innerHTML="You will need"+" "+ka+"KVA"+" "+"Inverter";
+        if((load && hrs)){
+    document.getElementById('sPanel').innerHTML="You will need "+solarPanel+"watts solar panel";
+    document.getElementById('batbank').innerHTML="You will need "+batteryBank+"ah 12v battery";
+    document.getElementById('chargecontrol').innerHTML="You will need "+chargeController+"amps charge controller";
+    document.getElementById('inverter').innerHTML="You will need "+inverterW+"watts Inverter OR";
+    document.getElementById('inverterVb').innerHTML="You will need "+kva+"Kva Inverter which is the same as "+inverterVa+"va inverter";
     document.getElementById('call').innerHTML="Requirements";
-    document.getElementById('note').innerHTML="Pls note, if you added a refrigerator and a pressing iron as appliances to be use with the solar, you will need x2 of the above inverter(Kva) size due to surge";
+    document.getElementById('note').innerHTML="Pls note, if you added a refrigerator and a pressing iron as appliances to be used on the solar system, you will need x2 of the above recommended inverter(Kva) size due to their surge";
+    document.getElementById("empty").style.display = "none";
     document.getElementById('inquire').innerHTML="For more enquiries on specific products to buy, pls chat 08106264634 on whatsapp.";
+        }else{
+    document.getElementById('empty').innerHTML="The fields are empty!!!";
+    };
     };
 
